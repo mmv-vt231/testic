70b9ff7b2cf0c96739d45f302fce6aaa8f9b165e
Mykhailo <vt231_mmv@student.ztu.edu.ua>
2024-05-15 14:13:20 +0300

    Add request DTO validation

--- Contracts/Tasks/StartTaskRequestDTO.cs
+++ Contracts/Tasks/StartTaskRequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 namespace Contracts.Tasks
 {
 	public record StartTaskRequestDTO(
+		[Required, EmailAddress, StringLength(255)]
 		string Email
 	);
 }
--- Contracts/Tasks/UpdateTaskRequestDTO.cs
+++ Contracts/Tasks/UpdateTaskRequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,12 +8,25 @@ using System.Threading.Tasks;
 namespace Contracts.Tasks
 {
 	public record UpdateTaskRequestDTO(
+		[Required]
 		ICollection<Guid> Groups,
+
+		[Required]
 		DateTime Start,
+
+		[Required]
 		DateTime End,
+
+		[Required, Range(1, 300)]
 		int Duration,
+
+		[Required]
 		bool OneChance,
+
+		[Required]
 		bool ShowAnswers,
+
+		[Required]
 		bool ShuffleQuestions
 	);
 }
